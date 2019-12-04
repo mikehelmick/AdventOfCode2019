fc8f370ecdbcf4f526a64f87d6d8b4db02dd6212
Mike Helmick <helmick@google.com>
2019-12-03 22:07:05 -0800

    clean up day 4 solutions, better naming, etc

--- day04/puzzle2.ex
+++ day04/puzzle2.ex
@@ -1,42 +1,24 @@
 defmodule Puzzle do
 
-  def to_list(num) do
-    [div(num,             100000),
-     div(rem(num, 100000), 10000),
-     div(rem(num, 10000),   1000),
-     div(rem(num, 1000),     100),
-     div(rem(num, 100),       10),
-     rem(num, 10),]
-  end
+  def ascending([a,b,c,d,e,f]) when b >= a and c >= b and d >= c and e >= d and f >= e, do: true
+  def ascending(_), do: false
 
-  def rule1([x,x,_,_,_,_]), do: true
-  def rule1([_,x,x,_,_,_]), do: true
-  def rule1([_,_,x,x,_,_]), do: true
-  def rule1([_,_,_,x,x,_]), do: true
-  def rule1([_,_,_,_,x,x]), do: true
-  def rule1(_), do: false
-
-  def rule2([a,b,c,d,e,f]) when b >= a and c >= b and d >= c and e >= d and f >= e, do: true
-  def rule2(_), do: false
-
-  def rule3([a,a,b,_,_,_]) when a != b, do: true
-  def rule3([a,b,b,c,_,_]) when a != b and b != c, do: true
-  def rule3([_,a,b,b,c,_]) when a != b and b != c, do: true
-  def rule3([_,_,a,b,b,c]) when a != b and b != c, do: true
-  def rule3([_,_,_,a,b,b]) when a != b, do: true
-  def rule3(_), do: false
+  def short_pair([a,a,b,_,_,_]) when a != b, do: true
+  def short_pair([a,b,b,c,_,_]) when a != b and b != c, do: true
+  def short_pair([_,a,b,b,c,_]) when a != b and b != c, do: true
+  def short_pair([_,_,a,b,b,c]) when a != b and b != c, do: true
+  def short_pair([_,_,_,a,b,b]) when a != b, do: true
+  def short_pair(_), do: false
 
   def solve(x, y, acc) when x > y, do: acc
   def solve(x, y, acc) do
-    digits = to_list(x)
-
-    case {rule1(digits), rule2(digits), rule3(digits)} do
-      {true, true, true} -> solve(x+1, y, acc ++ [x])
+    digits = Integer.digits(x)
+    case {short_pair(digits), ascending(digits)} do
+      {true, true} -> solve(x+1, y, acc ++ [x])
       _ -> solve(x+1, y, acc)
     end
   end
 end
-
 
 valid = Puzzle.solve(367479, 893698, [])
 IO.puts("Answer #{length(valid)}")
