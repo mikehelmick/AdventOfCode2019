302a90988248f33848a3a2c177c61be5445e5336
Mike Helmick <mike.helmick@gmail.com>
2019-12-21 19:39:53 -0800

    working, but very very inefficient day 18

--- day18/main.go
+++ day18/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"container/heap"
 	"fmt"
 	"log"
 	"os"
@@ -29,6 +28,14 @@ func (m *maze) print() {
 		}
 		fmt.Println()
 	}
+}
+
+func (m *maze) isDoor(p pos) bool {
+	return isDoor(m.grid[p])
+}
+
+func (m *maze) isKey(p pos) bool {
+	return isKey(m.grid[p])
 }
 
 func isDoor(s string) bool {
@@ -79,8 +86,9 @@ func enqueueNeighbors(p pos, m *maze, visited map[pos]bool, q []pos) []pos {
 }
 
 type dist struct {
-	p pos
-	d int
+	p       pos
+	d       int
+	reqKeys []string
 }
 
 type priorityQueue []*dist
@@ -112,29 +120,72 @@ func (pq *priorityQueue) Pop() interface{} {
 	return item
 }
 
-func (m *maze) countDistanceToOthers(p pos) []dist {
-	visited := make(map[pos]bool)
-	visited[p] = true
-	q := make([]pos, 0, 100)
-	q = enqueueNeighbors(p, m, visited, q)
-
-	rounds := 1
-
-	distance := make([]dist, 0, 100)
-
-	for len(q) > 0 {
-		//log.Printf("Queue: %v", q)
-		nQ := make([]pos, 0, 100)
-		for _, cand := range q {
-			val := m.grid[cand]
-			if isKey(val) || isDoor(val) {
-				distance = append(distance, dist{cand, rounds})
+func getNeighbors(p pos, m *maze) []pos {
+	neighbors := make([]pos, 0, 4)
+	for _, offset := range osets {
+		if neighbor := p.add(offset); neighbor.isValid(m) {
+			if m.grid[neighbor] != "#" {
+				neighbors = append(neighbors, neighbor)
 			}
-			nQ = enqueueNeighbors(cand, m, visited, nQ)
 		}
-		q = nQ
-		rounds++
 	}
+	return neighbors
+}
+
+func copyDoors(doors []string) []string {
+	cpy := make([]string, len(doors))
+	copy(cpy, doors)
+	return cpy
+}
+
+func (m *maze) distanceDFS(p pos, visited map[pos]bool, doorsInWay []string, curDist int, distance map[pos]dist) {
+	visited[p] = true
+	curDist++
+	for _, neighbor := range getNeighbors(p, m) {
+		if visited[neighbor] {
+			continue
+		}
+		if m.isDoor(neighbor) || m.isKey(neighbor) {
+			// record this door or key
+			if m.isDoor(neighbor) {
+				doorsInWay = append(copyDoors(doorsInWay), m.grid[neighbor])
+			}
+			distance[neighbor] = dist{neighbor, curDist, copyDoors(doorsInWay)}
+			m.distanceDFS(neighbor, visited, doorsInWay, curDist, distance)
+		} else {
+			// traverse this node
+			m.distanceDFS(neighbor, visited, doorsInWay, curDist, distance)
+		}
+	}
+
+	delete(visited, p)
+}
+
+func (m *maze) countDistanceToOthers(p pos) map[pos]dist {
+	log.Printf("Calculating adjency for %v @ %v", m.grid[p], p)
+	visited := make(map[pos]bool)
+	//enqueueNeighbors(p, m, visited, q)
+
+	distance := make(map[pos]dist)
+	doorsInWay := make([]string, 0, 100)
+	m.distanceDFS(p, visited, doorsInWay, 0, distance)
+	log.Printf("Adjacency for %v @ %v: %v", p, m.grid[p], distance)
+
+	/*
+		for len(q) > 0 {
+			//log.Printf("Queue: %v", q)
+			nQ := make([]pos, 0, 100)
+			for _, cand := range q {
+				val := m.grid[cand]
+				if isKey(val) || isDoor(val) {
+					distance = append(distance, dist{cand, rounds})
+				}
+				nQ = enqueueNeighbors(cand, m, visited, nQ)
+			}
+			q = nQ
+			rounds++
+		}
+	*/
 	return distance
 }
 
@@ -147,6 +198,7 @@ func (m *maze) printCosts(cost map[pos]int) {
 // INFINITY is just a big number
 const INFINITY = 32000000
 
+/*
 func (m *maze) solve(p pos, doors, keys map[pos]string) int {
 	cost := make(map[pos]int)
 
@@ -226,14 +278,108 @@ func (m *maze) solve(p pos, doors, keys map[pos]string) int {
 
 	return maxCost
 }
+*/
 
-type record struct {
-	key   pos
-	doors []pos
+func hasKey(m *maze, heldKeys map[string]bool, p pos) bool {
+	return heldKeys[m.grid[p]]
 }
 
-func (m *maze) solve2(p pos, doors, keys map[pos]string) int {
+func pickupKey(m *maze, heldKeys map[string]bool, p pos) {
+	heldKeys[strings.ToUpper(m.grid[p])] = true
+}
 
+func dropKey(m *maze, heldKeys map[string]bool, p pos) {
+	delete(heldKeys, strings.ToUpper(m.grid[p]))
+}
+
+func hasNecessaryKeys(heldKeys map[string]bool, doors []string) bool {
+	for _, key := range doors {
+		if !heldKeys[key] {
+			return false
+		}
+	}
+	return true
+}
+
+func (m *maze) printStack(stack []pos) {
+	s := ""
+	for _, p := range stack {
+		s = fmt.Sprintf("%s -> %s", s, m.grid[p])
+	}
+	log.Printf("%s\n", s)
+}
+
+func (m *maze) runDfs(stack []pos, visited map[pos]bool, heldKeys map[string]bool, target int, distTo map[pos]map[pos]dist, dist int, toBeat int) int {
+	if dist > toBeat {
+		return toBeat
+	}
+	p := stack[len(stack)-1]
+	if len(heldKeys) == target {
+		//log.Printf("Searching from %v -> %v is last key at dist %v", p, m.grid[p], dist)
+		return dist
+	}
+	visited[p] = true
+	//log.Printf("%v :: Searching from %v -> %v, dist:%v", stack, p, m.grid[p], dist)
+	//log.Printf("Adjacency: %v", distTo[p])
+	result := toBeat
+
+	for _, dst := range distTo[p] {
+		if visited[dst.p] {
+			continue
+		}
+		//log.Printf(" \\-> Considering %v -> %v, haveKeys %v", dst, m.grid[dst.p], heldKeys)
+		if m.isDoor(dst.p) || m.isKey(dst.p) {
+			if !hasNecessaryKeys(heldKeys, dst.reqKeys) {
+				//log.Printf(" \\-> Don't have necessary key for %v -> %v", dst.p, m.grid[dst.p])
+			} else {
+				if m.isKey(dst.p) {
+					pickupKey(m, heldKeys, dst.p)
+				}
+				newStack := make([]pos, len(stack)+1)
+				copy(newStack, stack)
+				newStack[len(stack)] = dst.p
+
+				stop := make(chan int)
+				go func() {
+					stop <- m.runDfs(newStack, visited, heldKeys, target, distTo, dist+dst.d, toBeat)
+				}()
+				res := <-stop
+				//} else {
+				//	m.runDfs(newStack, visited, heldKeys, target, distTo, dist+dst.d, toBeat)
+				//}
+
+				if res < result {
+					if len(heldKeys) == target {
+						log.Printf("New shortest path: %v", res)
+						//m.printStack(stack)
+					}
+					result = res
+					toBeat = res
+				}
+				if m.isKey(dst.p) {
+					dropKey(m, heldKeys, dst.p)
+				}
+			}
+		}
+	}
+	delete(visited, p)
+	return result
+}
+
+// Solve using DFS
+func (m *maze) solve2(p pos, doors, keys map[pos]string) int {
+	visited := make(map[pos]bool)
+	heldKeys := make(map[string]bool)
+	distTo := make(map[pos]map[pos]dist)
+	for k, v := range m.grid {
+		if v == "@" || isDoor(v) || isKey(v) {
+			distTo[k] = m.countDistanceToOthers(k)
+		}
+	}
+	log.Printf("----------------------------------")
+	stack := make([]pos, 1)
+	stack[0] = p
+	return m.runDfs(stack, visited, heldKeys, len(keys), distTo, 0, 8750)
 }
 
 func main() {
@@ -256,6 +402,6 @@ func main() {
 	fmt.Printf("keys (%v): %v\n", len(keys), keys)
 	fmt.Printf("doors (%v): %v\n", len(doors), doors)
 	fmt.Println("---solving---")
-	steps := maze.solve(*origin, doors, keys)
+	steps := maze.solve2(*origin, doors, keys)
 	fmt.Printf("Steps needed %v\n", steps)
 }
